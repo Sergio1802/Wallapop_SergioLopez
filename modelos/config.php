134949edbe01ab4dc5e4ffa8f4b7e18df34a787d
Sergio1802 <48331824+Sergio1802@users.noreply.github.com>
2024-01-16 20:43:23 +0100

    Update config.php

--- modelos/config.php
+++ modelos/config.php
@@ -1,5 +1,9 @@
 <?php 
-define('MYSQL_USER', 'Sergio');
-define('MYSQL_PASS', 'root');
-define('MYSQL_DB', 'wallapop');
-define('MYSQL_HOST', 'localhost');
+$MYSQL_HOST=getenv("DB_HOST");
+$MYSQL_DB=getenv("DB_NAME");
+$MYSQL_PASS=getenv("DB_PASSWORD");
+$MYSQL_USER=getenv("DB_USERNAME");
+//define('MYSQL_USER', 'Sergio');
+//define('MYSQL_PASS', 'root');
+//define('MYSQL_DB', 'wallapop');
+//define('MYSQL_HOST', 'localhost');
