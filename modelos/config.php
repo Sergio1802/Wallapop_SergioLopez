d31b77100ce2767cbcef5b1e5de845b7bf4fbc8e
Sergio1802 <48331824+Sergio1802@users.noreply.github.com>
2024-01-17 16:27:30 +0100

    Update config.php

--- modelos/config.php
+++ modelos/config.php
@@ -1,11 +1,6 @@
 <?php 
-$MYSQL_HOST=getenv("DB_HOST");
-$MYSQL_DB=getenv("DB_NAME");
-$MYSQL_PASS=getenv("DB_PASSWORD");
-$MYSQL_USER=getenv("DB_USERNAME");
 
-$conect=mysqli_connect($MYSQL_HOST,$MYSQL_USER,$MYSQL_PASS,$MYSQL_DB);
-//define('MYSQL_USER', 'Sergio');
-//define('MYSQL_PASS', 'root');
-//define('MYSQL_DB', 'wallapop');
-//define('MYSQL_HOST', 'localhost');
+define('MYSQL_USER', 'avgcqjcavk');
+define('MYSQL_PASS', '2HF0UM3X2PM82COC$');
+define('MYSQL_DB', 'sergiolopeztarea-database');
+define('MYSQL_HOST', 'sergiolopeztarea-server.mysql.database.azure.com');
